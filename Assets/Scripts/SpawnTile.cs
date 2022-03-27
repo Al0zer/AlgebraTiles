7c2e619e8a51f264303fa6e8023f882e6a1b1642
AaronLi <dumfing@gmail.com>
2022-03-27 14:44:11 -0400

    Add clicking in workspace restriction

--- Assets/Scripts/SpawnTile.cs
+++ Assets/Scripts/SpawnTile.cs
@@ -6,8 +6,11 @@ public class SpawnTile : MonoBehaviour
 {
 
     public GameObject tile;
-    private bool inWorkSpace = false;
+
+    public GameObject square;
     Camera camera;
+
+
 
     public void Awake()
     {
@@ -20,11 +23,18 @@ public class SpawnTile : MonoBehaviour
         //spawn a tile where the mouse cursor is
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePos = Input.mousePosition;
-            mousePos.z = Mathf.Abs(camera.transform.position.z);
-            Vector3 tilePos = camera.ScreenToWorldPoint(mousePos);
-            tilePos.z = 0f;
-            Instantiate(tile, tilePos, Quaternion.identity);
+            // ray cast from the camera to the mouse position
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+
+            // modify this to allow multiple squares if necessary, or to allow changing which square is being checked
+            if(hit.collider == square.GetComponent<BoxCollider2D>()){
+                Vector3 mousePos = Input.mousePosition;
+                mousePos.z = Mathf.Abs(camera.transform.position.z);
+                Vector3 tilePos = camera.ScreenToWorldPoint(mousePos);
+                tilePos.z = -1f;
+                Instantiate(tile, tilePos, Quaternion.identity);
+            }   
         }
     }
 }
