b6ac5995de55cdbd832cd82c9915ea58bc4604e4
AaronLi <dumfing@gmail.com>
2022-03-28 21:26:53 -0400

    Merge SpawnTile script and TileButton script into TileCreationManager

---
+++ Assets/Scripts/TileCreationManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TileCreationManager : MonoBehaviour
+{
+    public GameObject onesTile;
+    public GameObject xTile;
+
+    public GameObject allowedArea;
+    private bool onesTileOn = false;
+    private bool xTileOn = false;
+
+    private Camera viewCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        viewCamera = Camera.main;
+    }
+
+    //if the ones tile button was pressed
+    public void OnesTile()
+    {
+        //if tiles button hasn't already been pressed
+        onesTileOn = !onesTileOn;
+        xTileOn = false;
+    }
+
+    //if the x tile button was pressed
+    public void XTile()
+    {
+        xTileOn = !xTileOn;
+        onesTileOn = false;
+    }
+
+    //remove all tiles from the workspace
+    public void ClearButton()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        foreach (GameObject obj in tiles)
+        {
+            Destroy(obj);
+        }
+    }
+
+    //return to start screen
+    public void HomeButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void Update()
+    {
+        //spawn a tile where the mouse cursor is
+        if (Input.GetMouseButtonDown(0) &&(onesTileOn || xTileOn))
+        {
+            // ray cast from the camera to the mouse position
+            Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+
+            // modify this to allow multiple squares if necessary, or to allow changing which square is being checked
+            if(hit.collider == allowedArea.GetComponent<BoxCollider2D>()){
+                Vector3 mousePos = Input.mousePosition;
+                mousePos.z = Mathf.Abs(viewCamera.transform.position.z);
+                Vector3 tilePos = viewCamera.ScreenToWorldPoint(mousePos);
+                tilePos.z = -1f;
+
+                if(onesTileOn){
+                    Instantiate(onesTile, tilePos, Quaternion.identity);
+                }else if(xTileOn){
+                    Instantiate(xTile, tilePos, Quaternion.identity);
+                }
+
+                //tile.tag = "Tile";
+            }   
+        }
+    }
+}
